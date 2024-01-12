3b67132fb351fe7607987270ab9e46adb6a376e4
Osa-nag00 <naghiseosa2000@gmail.com>
2024-01-12 17:11:41 -0500

    got threads to call func correctly

---
+++ Counter.hpp
@@ -0,0 +1,26 @@
+#include <mutex>
+
+class Counter
+{
+private:
+    std::mutex lock;
+    int counterVal;
+
+public:
+    Counter();
+    Counter(int n);
+    int getNextCounterVal();
+};
+
+Counter::Counter() {}
+
+Counter::Counter(int n)
+{
+    this->counterVal = n;
+}
+
+int Counter::getNextCounterVal()
+{
+    std::lock_guard<std::mutex> guard(lock);
+    return this->counterVal++;
+}
--- pa1.cpp
+++ pa1.cpp
@@ -3,13 +3,15 @@
 #include <bits/stdc++.h>
 #include <vector>
 #include <chrono>
+#include "Counter.hpp"
 
 #define MAX_PRIME_VAL 100000000
 #define CHUNK_SIZE 12500000
 #define TEST_MAX_PRIME_VAL 10000
 #define TEST_CHUNK_SIZE 2500
 
-std::vector<int> primes;
+Counter globalCounter(1);
+int myCount = 1;
 
 template <typename Func>
 double timeFunction(Func func, std::vector<int> vec)
@@ -61,6 +63,20 @@ std::vector<int> listOfPrimesNoMultithreading(std::vector<int> retval)
     return retval;
 }
 
+void printPrimes()
+{
+
+    while (myCount < MAX_PRIME_VAL)
+    {
+
+        myCount = globalCounter.getNextCounterVal();
+        if (isPrime(myCount))
+        {
+            printf("Prime: %d\n", myCount);
+        }
+    }
+}
+
 bool checkEqual(std::vector<int> a, std::vector<int> b)
 {
 
@@ -77,20 +93,28 @@ bool checkEqual(std::vector<int> a, std::vector<int> b)
 
 int main(int argc, char *argv[])
 {
+    int j;
+    int numThreads = 16;
+    std::vector<std::thread> threads;
 
-    std::vector<int> nonMulti;
-    std::vector<int> Multi;
+    for (j = 0; j < numThreads; j++)
+    {
+        // create threads
+        threads.push_back(std::thread(printPrimes));
+    }
 
-    double msNonMulti = timeFunction(listOfPrimesNoMultithreading, nonMulti);
-    // double msMulti = timeFunction(listOfPrimesMultithreading, Multi);
-    // std::vector<int> Multi = listOfPrimesMultithreading();
+    // sort(Multi.begin(), Multi.end());
 
-    sort(Multi.begin(), Multi.end());
+    // bool retval = checkEqual(Multi, nonMulti);
 
-    bool retval = checkEqual(Multi, nonMulti);
-
-    printf("Non Multi: %f milliseconds \n", msNonMulti);
+    // printf("Non Multi: %f milliseconds \n", msNonMulti);
     // printf(" Multi: %f milliseconds \n", msMulti);
     printf("%s", "Hello World!");
+
+    for (int x = 0; x < numThreads; x++)
+    {
+        threads.at(x).join();
+    }
+
     return 0;
 }
