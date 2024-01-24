61d10b9a2481d9b2bdc0804820aabd051460eefb
Osa-nag00 <naghiseosa2000@gmail.com>
2024-01-23 22:26:34 -0500

    added more efficient primes algorithm, removed shared counter, added output requirements

--- pa1.cpp
+++ pa1.cpp
@@ -1,86 +1,138 @@
-#include <stdio.h>
 #include <thread>
-#include <bits/stdc++.h>
 #include <vector>
 #include <chrono>
 #include <iostream>
-#include <functional>
-#include <iomanip>
-#include "Counter.hpp"
+#include <fstream>
+#include <string>
 
 #define MAX_PRIME_VAL 100000000
-#define TEST_MAX_PRIME_VAL 100000
+#define NUM_THREADS 8
+/*GLOBALS*/
+std::vector<int> primes(MAX_PRIME_VAL, 1); /*If prime, index will be 1, else 0*/
 
+double executionTimeSeconds;
+int totalNumberOfPrimes;
+long sumOfAllPrimes;
+std::vector<int> topTenPrimes;
+
+// function to get the runtime of function in seconds
 template <typename Func>
-double timeFunction(Func func, int numThreads)
+double timeFunction(Func func)
 {
     auto start = std::chrono::high_resolution_clock::now();
-    func(numThreads);
+    func();
     auto end = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double, std::milli> duration = end - start;
-    return duration.count();
+    return (duration.count() / 1000);
 }
 
-bool isPrime(int n)
+void sieveOfEratosthenes(int start, int end)
 {
-    // Corner case
-    if (n <= 1)
-        return false;
-
-    // Check from 2 to n-1
-    for (int i = 2; i <= n / 2; i++)
-        if (n % i == 0)
-            return false;
-
-    return true;
-}
-
-void printPrimes(Counter &counter, int i)
-{
-
-    while (i < MAX_PRIME_VAL)
+    for (int p = 2; p * p <= MAX_PRIME_VAL; ++p)
     {
-
-        i = counter.getNextCounterVal();
-        if (isPrime(i))
+        if (primes[p])
         {
-            printf("Prime: %d\n", i);
+            for (int i = std::max(p * p, (start + p - 1) / p * p); i <= end; i += p)
+            {
+                primes[i] = 0;
+            }
         }
     }
 }
 
-void printPrimesWrapper(int numThreads)
+void computePrimesParallel()
 {
-    int j;
-    Counter counter(1); /* custom increment counter that is multithreading safe, this is also shared between all the threads*/
-    std::vector<std::jthread> jthreads;
+    int i, j, start, end;
+    int segmentSize = MAX_PRIME_VAL / NUM_THREADS;
+    std::thread threads[NUM_THREADS];
 
-    for (j = 0; j < numThreads; j++)
+    for (i = 0; i < NUM_THREADS; i++)
     {
-        jthreads.push_back(std::jthread(printPrimes, std::ref(counter), 0));
+
+        // the index times the segment size is an easy
+        // way to get the starting position of a chunk
+        start = i * segmentSize + 1;
+        end = (i + 1) * segmentSize;
+
+        if (i == (NUM_THREADS - 1))
+        {
+            end = MAX_PRIME_VAL;
+        }
+
+        // creating threads
+        // assign them to an index
+        // send them the func and params to run
+        threads[i] = std::thread(sieveOfEratosthenes, start, end);
+    }
+
+    // stop and join all the threads here before continuing
+    for (j = 0; j < NUM_THREADS; j++)
+    {
+        threads[j].join();
     }
 }
 
-bool checkEqual(std::vector<int> a, std::vector<int> b)
+void generateTextFile(const std::string &filename, const std::string &content)
 {
+    std::ofstream file(filename);
+    file << content;
+    file.close();
+}
 
-    for (int i = 0; i < a.size() && i < b.size(); i++)
+void miscCalculations()
+{
+    int i, j;
+
+    int primesSize = primes.size();
+
+    // does not account for 1 and 0
+    for (i = 2; i < primesSize; i++)
     {
-        if (a.at(i) != b.at(i))
+        if (primes[i] == 1)
         {
-            return false;
+            totalNumberOfPrimes++;
+            sumOfAllPrimes += i;
         }
     }
 
-    return true;
+    j = 0;
+
+    while (topTenPrimes.size() < 10)
+    {
+        // starting from the top of the list count down
+        if (primes[(primesSize - 1) - j] == 1)
+        {
+            topTenPrimes.push_back((primesSize - 1) - j);
+        }
+        j++;
+    }
+
+    // the vector is collected from greatest to least, so its reversed here
+    std::reverse(topTenPrimes.begin(), topTenPrimes.end());
 }
 
 int main(int argc, char *argv[])
 {
 
-    double timeTaken = timeFunction(printPrimesWrapper, 8);
+    double executionTimeSeconds = timeFunction(computePrimesParallel);
+    // std::cout << "Time Seconds: " << executionTimeSeconds << std::endl;
+    miscCalculations();
 
-    std::cout << "Time Taken: " << std::setprecision(2) << timeTaken / 1000 << " seconds" << std::endl;
+    std::string topTenPrimesString;
+    topTenPrimesString += "[";
+    for (int prime : topTenPrimes)
+    {
+        topTenPrimesString += std::to_string(prime) + ", ";
+    }
+    topTenPrimesString += "]";
+
+    std::string outputString = "Execution time: " + std::to_string(executionTimeSeconds) + " seconds" + "\n" +
+                               "Total number of primes found: " + std::to_string(totalNumberOfPrimes) + "\n" +
+                               "Sum of all primes found: " + std::to_string(sumOfAllPrimes) + "\n" +
+                               "Top ten maximum primes: " + topTenPrimesString + "\n";
+
+    generateTextFile("primes.txt", outputString);
+
     return 0;
 }
